3bcfded9cba352e55a2b2e7701082a61eb201b15
Veronica Prieto <131327328+VeronicaPrieto@users.noreply.github.com>
2024-04-20 03:01:41 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -3,41 +3,59 @@ let diccionario = ['APPLE', 'HURLS', 'WINGS', 'YOUTH', "ABIDE", "ABUSE", "ADMIT"
     "AWAKE", "BLEED", "BLESS", "BRAKE", "BLINK", "CARRY", "CATCH", "CAUSE", "CHOKE", "CLEAN", "CLAIM", "CROSS",
     "DANCE", "DRINK", "DREAM", "EJECT", "EXIST", "FOUND", "GRADE", "GUESS", "JUDGE", "LEARN", "LOWER", "MOUNT", "PLACE",
     "RAISE", "RHYME", "SCREW", "SMELL", "TASTE", "TRADE", "UPSET", "WASTE", "WRITE", "WATER"];
+
 const palabra = diccionario[Math.floor(Math.random() * diccionario.length)];
-window.addEventListener('load', init)
-function init() {
-    console.log('Esto se ejecuta solo cuando se carga la pagina web')
-}
 const button = document.getElementById("guess-button");
-button.addEventListener("click", intentar);
 const input = document.getElementById("guess-input");
-const valor = input.value;
+const playAgainButton = document.getElementById("play-again-button");
+
+window.addEventListener('load', init);
+
+function init() {
+    console.log('Esto se ejecuta solo cuando se carga la pagina web',palabra);
+    input.addEventListener("keypress",function(event){
+        if(event.key === "Enter"){
+            intentar();
+            input.value="";
+        }
+    });
+    button.addEventListener("click",function(){
+        intentar();
+        input.value="";
+    });
+}
+
 
 function intentar() {
     const INTENTO = leerIntento();
-    if (!INTENTO) {
+    if (!INTENTO || INTENTO.length<5 || INTENTO.length>5) {
+        alert("Ingresa una palabra de 5 letras.")
         return;
     }
     const GRID = document.getElementById("grid");
     const ROW = document.createElement('div');
     ROW.className = 'row';
-
-    for (let i in palabra) {
+    const letrasAdivinadas = {};
+    for (let i = 0; i < palabra.length; i++) {
+        const letraDeLaPalabra = palabra[i];
         const SPAN = document.createElement('span');
         SPAN.className = 'letter';
-        if (INTENTO[i] === palabra[i]) {
-            SPAN.innerHTML = INTENTO[i];
+        const letraEncontrada = INTENTO.includes(letraDeLaPalabra);
+        const letraCorrecta = (INTENTO[i] === letraDeLaPalabra);
+        if (letraCorrecta) {
+            SPAN.innerHTML = letraDeLaPalabra;
             SPAN.style.backgroundColor = "#79b851";
-        } else if (palabra.includes(INTENTO[i])) {
+        } else if (letraEncontrada && !letrasAdivinadas[letraDeLaPalabra]) {
             SPAN.innerHTML = INTENTO[i];
             SPAN.style.backgroundColor = "#f3c237";
+            letrasAdivinadas[letraDeLaPalabra] = true;
         } else {
             SPAN.innerHTML = INTENTO[i];
             SPAN.style.backgroundColor = "#a4aec4";
         }
         ROW.appendChild(SPAN);
     }
-    GRID.appendChild(ROW)
+    GRID.appendChild(ROW);
     intentos--;
     if (intentos === 0 || INTENTO === palabra) {
         terminar(INTENTO === palabra ? "<h1>GANASTE!</h1>" : "<h1>PERDISTE!</h1>");
@@ -51,6 +69,14 @@ function leerIntento() {
 }
 function terminar(mensaje) {
     const INPUT = document.getElementById("guess-input");
+    const BUTTON = document.getElementById("guess-button");
     let contenedor = document.getElementById('guesses');
     contenedor.innerHTML = mensaje;
+    playAgainButton.style.display = "block";
+    playAgainButton.addEventListener("click", function () {
+        window.location.reload();
+    });
+    INPUT.disabled = true;
+    BUTTON.disabled = true;
+    BUTTON.style.cursor = "default";
 }
