98a6118b7dae5d2d7cf615fbb9f8125255fcb867
Veronica Prieto <131327328+VeronicaPrieto@users.noreply.github.com>
2024-04-20 01:01:57 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,54 +1,56 @@
 let intentos = 6;
-let diccionario = ['APPLE', 'HURLS', 'WINGS', 'YOUTH'];
+let diccionario = ['APPLE', 'HURLS', 'WINGS', 'YOUTH', "ABIDE", "ABUSE", "ADMIT", "ADORE", "AGREE", "ALLOW",
+    "AWAKE", "BLEED", "BLESS", "BRAKE", "BLINK", "CARRY", "CATCH", "CAUSE", "CHOKE", "CLEAN", "CLAIM", "CROSS",
+    "DANCE", "DRINK", "DREAM", "EJECT", "EXIST", "FOUND", "GRADE", "GUESS", "JUDGE", "LEARN", "LOWER", "MOUNT", "PLACE",
+    "RAISE", "RHYME", "SCREW", "SMELL", "TASTE", "TRADE", "UPSET", "WASTE", "WRITE", "WATER"];
 const palabra = diccionario[Math.floor(Math.random() * diccionario.length)];
-function init(){
-    console.log("El juego empezó");
+window.addEventListener('load', init)
+function init() {
+    console.log('Esto se ejecuta solo cuando se carga la pagina web')
 }
-function leerIntento(){
-    return document.getElementById("guess-input").value.toUpperCase();
-}
-function intentar(){
-    const GRID = document.getElementById("grid");
-    const ROW = document.createElement("div");
-    ROW.className = "row";
+const button = document.getElementById("guess-button");
+button.addEventListener("click", intentar);
+const input = document.getElementById("guess-input");
+const valor = input.value;
+
+function intentar() {
     const INTENTO = leerIntento();
-    if (INTENTO === palabra ) {
-        terminar("<h1>GANASTE!😀</h1>")
-        return
+    if (!INTENTO) {
+        return;
     }
-    for (let i in palabra){
-        const SPAN = document.createElement("span");
-        SPAN.className = "letter";
-        if (INTENTO[i] === palabra[i]){
+    const GRID = document.getElementById("grid");
+    const ROW = document.createElement('div');
+    ROW.className = 'row';
+
+    for (let i in palabra) {
+        const SPAN = document.createElement('span');
+        SPAN.className = 'letter';
+        if (INTENTO[i] === palabra[i]) {
             SPAN.innerHTML = INTENTO[i];
             SPAN.style.backgroundColor = "#79b851";
-        }
-        else if(palabra.includes(INTENTO[i])){
+        } else if (palabra.includes(INTENTO[i])) {
             SPAN.innerHTML = INTENTO[i];
             SPAN.style.backgroundColor = "#f3c237";
-        }
-        else{
+        } else {
             SPAN.innerHTML = INTENTO[i];
-            SPAN.style.backgroundColor = "a4aec4";
+            SPAN.style.backgroundColor = "#a4aec4";
         }
         ROW.appendChild(SPAN);
     }
-    GRID.appendChild(ROW);
+    GRID.appendChild(ROW)
     intentos--;
-    if (intentos==0){
-        terminar("<h1>PERDISTE!😖</h1>")
+    if (intentos === 0 || INTENTO === palabra) {
+        terminar(INTENTO === palabra ? "<h1>GANASTE!</h1>" : "<h1>PERDISTE!</h1>");
     }
 }
-function terminar(mensaje){
+function leerIntento() {
+    let intento = document.getElementById("guess-input");
+    intento = intento.value;
+    intento = intento.toUpperCase();
+    return intento;
+}
+function terminar(mensaje) {
     const INPUT = document.getElementById("guess-input");
-    INPUT.disabled = true;
-    BOTON.disabled = true;
-    let contenedor = document.getElementById("guesses");
+    let contenedor = document.getElementById('guesses');
     contenedor.innerHTML = mensaje;
 }
-window.addEventListener("load",init);
-
-const button = document.getElementById("guess-button");
-button.addEventListener("click",intentar);
-const input = document.getElementById("guess-input");
-const valor = input.value;
